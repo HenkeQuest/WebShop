422d97ebf4bcf948dbc6a4fda75fc2fb4ab6b3f1
eriol726 <eriol726@student.liu.se>
2019-06-25 22:45:06 +0200

    started with auth implementation

---
+++ WebAPIAngular/Models/AuthentificationContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIAngular.Models
+{
+    public class AuthentificationContext : IdentityDbContext
+    {
+        public AuthentificationContext(DbContextOptions options):base(options)
+        {
+
+        }
+    }
+}
