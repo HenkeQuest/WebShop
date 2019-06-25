38cb7c66ace46eb9104da1a7819804f0d3ae8180
eriol726 <eriol726@student.liu.se>
2019-06-25 09:03:15 +0200

    init git

---
+++ Angular7/src/app/records/record-list/record-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { RecordService } from 'src/app/shared/record.service';
+import { Record } from 'src/app/shared/record.model';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-record-list',
+  templateUrl: './record-list.component.html',
+  styleUrls: ['./record-list.component.css']
+})
+export class RecordListComponent implements OnInit {
+
+  constructor(private service : RecordService,
+    private toastr : ToastrService) { }
+
+  ngOnInit() {
+    this.service.refreshList();
+  }
+
+  populateForm(rec : Record){
+    this.service.formData = Object.assign({}, rec);
+  }
+
+  onDelete(id : number){
+    if(confirm('Are you sure to delete this record?')){
+      this.service.deleteRecord(id).subscribe(res=>{
+        this.toastr.warning("Deleted successfully", 'EMP. Register');
+        this.service.refreshList();
+      })
+    }
+  }
+
+}
