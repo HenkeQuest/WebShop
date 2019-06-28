527a4fdcf8a49c59242aa2410fbc7f9e83057454
eriol726 <eriol726@student.liu.se>
2019-06-28 08:04:45 +0200

    added creat user project

---
+++ WebApplication1_delete/WebApplication1_delete/Controllers/ApplicationUserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1_delete.Models;
+
+namespace WebApplication1_delete.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private UserManager<ApplicationUser> _signInManager;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, UserManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        //POST : api/ApplicationUser/Register
+        public async Task<Object> PostApplicationUser(ApplicationUserModel model)
+        {
+            var applicationUser = new ApplicationUser() {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
