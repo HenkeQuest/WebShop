fd372553344dd6849a7a48bacbb1a2e39e6a44c7
eriol726 <eriol726@student.liu.se>
2019-07-01 21:43:52 +0200

    Role Based Authorization implemented

--- WebApplication1_delete/WebApplication1_delete/Controllers/ApplicationUserController.cs
+++ WebApplication1_delete/WebApplication1_delete/Controllers/ApplicationUserController.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using WebApplication1_delete.Models;
 
 namespace WebApplication1_delete.Controllers
@@ -14,12 +19,14 @@ namespace WebApplication1_delete.Controllers
     public class ApplicationUserController : ControllerBase
     {
         private UserManager<ApplicationUser> _userManager;
-        private UserManager<ApplicationUser> _signInManager;
+        private SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationSettings _appSetting;
 
-        public ApplicationUserController(UserManager<ApplicationUser> userManager, UserManager<ApplicationUser> signInManager)
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IOptions<ApplicationSettings> appSettings)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _appSetting = appSettings.Value;
         }
 
         [HttpPost]
@@ -27,6 +34,7 @@ namespace WebApplication1_delete.Controllers
         //POST : api/ApplicationUser/Register
         public async Task<Object> PostApplicationUser(ApplicationUserModel model)
         {
+            model.Role = "Admin";
             var applicationUser = new ApplicationUser() {
                 UserName = model.UserName,
                 Email = model.Email,
@@ -36,12 +44,45 @@ namespace WebApplication1_delete.Controllers
             try
             {
                 var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                await _userManager.AddToRoleAsync(applicationUser, model.Role);
                 return Ok(result);
             }
             catch (Exception)
             {
 
                 throw;
+            }
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        //POST : /api/ApplicationUser/Login
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if(user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                //Get role assigned to user
+                var role = await _userManager.GetRolesAsync(user);
+                IdentityOptions _options = new IdentityOptions();
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim("UserID", user.Id.ToString()),
+                        new Claim(_options.ClaimsIdentity.RoleClaimType,role.FirstOrDefault())
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSetting.JWT_Secret)), SecurityAlgorithms.HmacSha256)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                var token = tokenHandler.WriteToken(securityToken);
+                return Ok(new { token });
+            }
+            else
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
             }
         }
 
