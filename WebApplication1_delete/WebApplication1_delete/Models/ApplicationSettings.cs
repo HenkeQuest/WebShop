996f2f5561620dcffb4a2b5bf14a5adc00f448b9
eriol726 <eriol726@student.liu.se>
2019-07-01 21:43:52 +0200

    Role Based Authorization implemented

---
+++ WebApplication1_delete/WebApplication1_delete/Models/ApplicationSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1_delete.Models
+{
+    public class ApplicationSettings
+    {
+        public string JWT_Secret { get; set; }
+        public string Clinet_URL { get; set; }
+    }
+}
