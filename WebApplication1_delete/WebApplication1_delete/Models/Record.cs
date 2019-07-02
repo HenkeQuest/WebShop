c2326aea253788fbd1914a9fc043ad2a22b63a71
eriol726 <eriol726@student.liu.se>
2019-07-02 23:33:09 +0200

    a string array can be send from record controller till record service

---
+++ WebApplication1_delete/WebApplication1_delete/Models/Record.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1_delete.Models
+{
+    public class Record
+    {
+        [Key]
+        public int RecordID { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        [Required]
+        public string Band { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Album { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public int Year { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        public string Genre { get; set; }
+    }
+}
