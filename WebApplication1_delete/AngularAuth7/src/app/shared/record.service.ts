eb90f68cde375cb95f5267e1b1e704754c559422
eriol726 <eriol726@student.liu.se>
2019-07-08 07:52:21 +0200

    working with image uploading

--- WebApplication1_delete/AngularAuth7/src/app/shared/record.service.ts
+++ WebApplication1_delete/AngularAuth7/src/app/shared/record.service.ts
@@ -12,16 +12,16 @@ export class RecordService {
   readonly rootURL = "http://localhost:62921/api"
   constructor(private http : HttpClient) { }
 
-  postRecord(formData : Record){
+  postRecord(formData : Record,fileToUpload: File ){
+    const formData1: FormData = new FormData;
+    //formData.Image = fileToUpload;
+    formData1.append("Band", "fdvfdg");
+    formData1.append("Album", "fdvfdg");
+    formData1.append("Year", "fdvfdg");
+    //formData1.append("Image", fileToUpload, "imageName");
     console.log("formData: ", formData);
+    console.log("fileToUpload: ", fileToUpload);
     return this.http.post(this.rootURL+"/Record", formData)
-  }
-
-  refreshList(){
-    this.http.get(this.rootURL+"/Record").toPromise().then(res => {
-      console.log("Res: ", res);
-      this.list = res as Record[];
-    });
   }
 
   putRecord(formData : Record){
@@ -30,5 +30,12 @@ export class RecordService {
 
   deleteRecord(id: number){
     return this.http.delete(this.rootURL+"/Record/"+id);
+  }
+
+  refreshList(){
+    this.http.get(this.rootURL+"/Record").toPromise().then(res => {
+      console.log("Res: ", res);
+      this.list = res as Record[];
+    });
   }
 }
