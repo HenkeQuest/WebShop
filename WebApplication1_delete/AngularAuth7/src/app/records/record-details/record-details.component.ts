30f7781736ad30a84f7403c39bffaa37144aea10
eriol726 <eriol726@student.liu.se>
2019-07-15 13:07:54 +0200

    images are editable

---
+++ WebApplication1_delete/AngularAuth7/src/app/records/record-details/record-details.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Record } from 'src/app/shared/record.model';
+import { RecordService } from 'src/app/shared/record.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-record-details',
+  templateUrl: './record-details.component.html',
+  styles: []
+})
+export class RecordDetailsComponent implements OnInit {
+
+  record: Record;
+  constructor(private route: ActivatedRoute, private service : RecordService) { }
+
+  ngOnInit() {
+    console.log("list: " + this.service.list);
+    this.route.params.subscribe(params => {
+      this.service.list.forEach((rec: Record) => {
+        console.log("rec: " + rec.RecordID);
+        if (rec.RecordID == params.id) {
+          this.record = rec;
+        }
+      });
+    });
+  }
+
+}
